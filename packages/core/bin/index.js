c76b29628b1d44dc7cda9ad6232cb68aec292cc3
minmin <1056640663@qq.com>
2022-11-02 22:32:17 +0800

    feat: file: ../utils

---
+++ packages/core/bin/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const utils = require('@snowlepoard520/utils');
+
+console.log('core', utils, 1111);
+
+function core() {
+  console.log('core', 1111);
+}
+
+
+
+// module.exports = core;
