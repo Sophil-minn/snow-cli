805b45adaf21cfd49d9e40e1eb458a13d781c4c7
minmin <1056640663@qq.com>
2022-11-05 23:00:47 +0800

    feat: 最低Node版本检查功能开发

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -4,15 +4,38 @@ module.exports = core;
 
 const pkg = require('../package-lock.json');
 const log = require('@snowlepoard520/log');
+const semver = require('semver');
+const colors = require('colors/safe');
+
+const constant = require('./const');
 
 function core() {
-  // 检查版本号
-  checkPkgVersion();
+  try {
+    // 检查版本号
+    checkPkgVersion();
+    checkNodeVersion();
+  } catch (e) {
+    console.log('我是铺货的错误');
+    log.error(e.message);
+  }
+  
   
 }
 
 function checkPkgVersion() {
   // TODO
   console.log( '版本号 ：', pkg.version);
-  log();
+  // log();
+}
+
+function checkNodeVersion() {
+  // 获取当前版本号
+  console.log(process.version, '当前node版本号');
+  const currentVersion = process.version;
+  const lowestVersion = constant.LOWEST_NODE_VERSION;
+  // 比对最低版本号
+  if (semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(colors.red(`snow-cli 需要安装v${lowestVersion}以上版本的node`));
+  }
+
 }
--- utils/log/lib/index.js
+++ utils/log/lib/index.js
@@ -12,7 +12,7 @@ log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL: 'info'; // 判断debu
 log.heading = 'snow-cli'; // 修改前缀
 log.addLevel('success', 2000, { fg: 'green', bold: true}); // 添加 自定义命令
 
-module.exports = index;
+module.exports = log;
 
 function index() {
   console.log('process.env.LOG_LEVEL: ', process.env.LOG_LEVEL);
