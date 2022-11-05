cf16ef8c7072baf24a8df8c1f1f3616fc66a7ed8
minmin <1056640663@qq.com>
2022-11-05 23:33:55 +0800

    feat: root账号启动检查和自动降级功能 及 用户主目录检查功能

--- core/cli/lib/const.js
+++ core/cli/lib/const.js
@@ -1,4 +1,4 @@
-const LOWEST_NODE_VERSION = '9.0.0';
+const LOWEST_NODE_VERSION = 'v16.3.0';
 
 module.exports = {
   LOWEST_NODE_VERSION
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -6,6 +6,9 @@ const pkg = require('../package-lock.json');
 const log = require('@snowlepoard520/log');
 const semver = require('semver');
 const colors = require('colors/safe');
+const userHome = require('user-home');
+const pathExists = require('path-exists').sync;
+const rootCheck = require('root-check');
 
 const constant = require('./const');
 
@@ -14,6 +17,8 @@ function core() {
     // 检查版本号
     checkPkgVersion();
     checkNodeVersion();
+    checkRoot();
+    checkUserHome();
   } catch (e) {
     console.log('我是铺货的错误');
     log.error(e.message);
@@ -34,8 +39,21 @@ function checkNodeVersion() {
   const currentVersion = process.version;
   const lowestVersion = constant.LOWEST_NODE_VERSION;
   // 比对最低版本号
-  if (semver.gte(currentVersion, lowestVersion)) {
+  if (!semver.gte(currentVersion, lowestVersion)) {
     throw new Error(colors.red(`snow-cli 需要安装v${lowestVersion}以上版本的node`));
   }
 
+}
+
+function checkRoot() {
+  console.log('登陆者何人?', process.geteuid())
+  rootCheck(); // root 降级
+  console.log('降级后,登陆者何人?', process.geteuid())
+}
+
+function checkUserHome() {
+  console.log('userHome: ', userHome);
+  if (!userHome || !pathExists(userHome)) {
+    throw new Error(colors.red('当前登陆用户主目录不存在！'))
+  }
 }
