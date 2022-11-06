3b304f4ba75d26490bd52db33612c45ce84050d6
minmin <1056640663@qq.com>
2022-11-06 09:12:42 +0800

    feat: 环境变量检查功能开发

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -3,22 +3,33 @@
 module.exports = core;
 
 const pkg = require('../package-lock.json');
-const log = require('@snowlepoard520/log');
+
 const semver = require('semver');
 const colors = require('colors/safe');
 const userHome = require('user-home');
 const pathExists = require('path-exists').sync;
 const rootCheck = require('root-check');
-
+const log = require('@snowlepoard520/log');
 const constant = require('./const');
+
+let args;
+
+// checkInputArgs();
+
+
 
 function core() {
   try {
     // 检查版本号
     checkPkgVersion();
     checkNodeVersion();
+    // 用户权限降级
     checkRoot();
+    // 检查用户主目录
     checkUserHome();
+    // 检查入参
+    checkInputArgs();
+    log.verbose('test', 'test debug log');
   } catch (e) {
     console.log('我是铺货的错误');
     log.error(e.message);
@@ -49,6 +60,22 @@ function checkRoot() {
   console.log('登陆者何人?', process.geteuid())
   rootCheck(); // root 降级
   console.log('降级后,登陆者何人?', process.geteuid())
+}
+
+function checkInputArgs() {
+  const minimist = require('minimist');
+  args = minimist(process.argv.slice(2));
+  console.log('args: ', args);
+  checkArgs();
+}
+
+function checkArgs() {
+  if (args.debug) {
+    process.env.LOG_LEVEL = 'verbose';
+  } else {
+    process.env.LOG_LEVEL = 'info';
+  }
+  log.level =  process.env.LOG_LEVEL;
 }
 
 function checkUserHome() {
