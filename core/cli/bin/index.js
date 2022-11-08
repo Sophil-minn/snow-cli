c9fc996586c335aee1e3093c22f6e008c980b628
minmin <1056640663@qq.com>
2022-11-08 22:55:00 +0800

    feat: lerna publish

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -2,18 +2,9 @@
 
 // const utils = require('@snowlepoard520/utils');
 
-// console.log('core++++++', utils, 1111);
-// utils();
-
-// function core() {
-//   console.log('core', 1111);
-// }
-
 // module.exports = core;
 
 // const importLocal = require('import-local');
-
-// console.log('__filename: ', __filename);
 
 if (importLocal(__filename)) {
   require('npmlog').info('cli', '正在使用 snow-cli 本地版本')
