dfa3b40c44d6e09b5f37b797fe79cc61a2675f44
minmin <1056640663@qq.com>
2022-11-10 23:33:30 +0800

    feat: version

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -1,15 +1,10 @@
 #! /usr/bin/env node
 
-// const utils = require('@snowlepoard520/utils');
+const utils = require('@snowlepoard520/utils');
 const log = require('@snowlepoard520/log');
-// const log = require('npmlog');
-
-// module.exports = core;
 
 const importLocal = require('import-local');
 console.log('__filename: ', new Date().getDate());
-
-console.log('1.2.5');
 
 if (importLocal(__filename)) {
   log.info('cli', '正在使用 snow-cli 本地版本')
