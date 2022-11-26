41e81640af8097ba388ba803adfe148f476bba1a
minmin <1056640663@qq.com>
2022-11-27 00:05:39 +0800

    feat: 提示优化

--- commands/init/lib/getProjectTemplate.js
+++ commands/init/lib/getProjectTemplate.js
@@ -1,5 +1,6 @@
 const request = require('@snowlepoard520/request');
 const log = require('@snowlepoard520/log');
+const colors = require('colors');
 
 async function getTemplate () {
   let result = [];
@@ -9,7 +10,13 @@ async function getTemplate () {
     });
     log.verbose('/snow/template ------  result: ', result);
   } catch (error) {
-    log.warnner('请求http://mac.minn.snowlepoard:7001/本地api接口失败: 启用兜底mock数据',);
+    log.warnner(colors.brightYellow('请求http://mac.minn.snowlepoard:7001/本地api接口失败'));
+    // log.warnner(colors.brightRed('启用兜底mock数据'));
+    // log.warnner(colors.brightBlue('请求http://mac.minn.snowlepoard:7001/本地api接口失败'));
+    log.warnner(colors.brightMagenta('重要提醒: 启用兜底mock数据'));
+    // log.warnner(colors.brightCyan('请求http://mac.minn.snowlepoard:7001/本地api接口失败'));
+    log.warnner(colors.brightWhite('重要⏰: 启用兜底mock数据'));
+    log.warnner(colors.brightGreen('重要提醒: 启用兜底mock数据'));
     result = [{
       version: '1.0.0',
       name: 'vue标准模板',
