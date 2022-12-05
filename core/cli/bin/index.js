7c7e4ed91322ec0829afec91ad3dbc3d37ba0afd
minmin <1056640663@qq.com>
2022-12-05 19:52:28 +0800

    feat: log

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -1,23 +1,22 @@
 #! /usr/bin/env node
 
 const colors = require('colors/safe')
-const log = require('@snowlepoard520/log');
 const importLocal = require('import-local');
 
 // console.log('time: ', new Date().getDate());
-log.snow(colors.bgBlack(colors.rainbow('welcome to snow-cli  -  snow lepoard Just to Run!')));
-log.snow(colors.brightGreen('contact me on github@Sophil-minn'));
-log.snow(colors.brightBlue('init... 请稍等'));
+console.log(colors.bgBlack(colors.rainbow('welcome to snow-cli  -  snow lepoard Just to Run!')));
+console.log(colors.brightGreen('contact me on github@Sophil-minn'));
+console.log(colors.brightBlue('init... 请稍等'));
 
 async function run() {
   if (importLocal(__filename)) {
-    log.verbose(colors.bgBlack(colors.brightGreen('you are using snow-cli local version +++++++++++++')))
+    console.log(colors.bgBlack(colors.brightGreen('you are using snow-cli local version +++++++++++++')))
   } else {
-    log.verbose(colors.bgBlack(colors.green('now using snow-cli development version ----------- ')))
-    log.verbose('执行流程准备中..., 请稍等...');
+    console.log(colors.bgBlack(colors.green('now using snow-cli development version ----------- ')))
+    console.log('执行流程准备中..., 请稍等...');
     require('../lib')(process.argv.slice(2));
   }
-  log.verbose('today is:', (new Date() + '').slice(0, 10))
+  console.log('today is:', (new Date() + '').slice(0, 10))
 }
 
 run()
